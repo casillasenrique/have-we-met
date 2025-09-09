b1b6756e917bdc269d780c3765fef8477ac67733
enrique <casillasenrique@live.com>
2025-09-08 22:45:16 -0400

    Add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import { getTodaysGameId } from "@/api/objectData";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>have we met</div>;
+  const todaysGameId = getTodaysGameId();
+  return (
+    <div
+      className="w-screen h-screen bg-cover bg-top max-w-[1024px] text-primary flex flex-col justify-center items-center text-6xl font-bold"
+      style={{ backgroundImage: "url('/madame-x.png')" }}
+    >
+      <div className="w-3/4 flex flex-col items-center gap-8 mb-32">
+        <div className="w-full">
+          <h1 className="py-1">have we</h1>
+          <h1 className="py-1">MET?</h1>
+        </div>
+        <Link className="w-full text-3xl flex" href={`/game/${todaysGameId}`}>
+          <p className="py-1">enter game </p>
+          <span className="material-icons text-primary text-[46px]!">
+            arrow_right_alt
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
 }
