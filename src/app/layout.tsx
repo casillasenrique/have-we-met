b3459fae43d2fe2fcbbd87c2613d72ca109a2574
enrique <casillasenrique@live.com>
2025-09-14 14:57:32 -0400

    Added analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./globals.css";
 import ClientLayout from "./components/ClientLayout";
 import { getTodaysGameId } from "@/api/objectData";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function RootLayout({
   children,
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body className="bg-white ">
         <div className="max-w-[1024px] min-h-screen mx-auto bg-white outline outline-primary">
           <ClientLayout todaysGameId={todaysGameId}>{children}</ClientLayout>
+          <Analytics />
         </div>
       </body>
     </html>
