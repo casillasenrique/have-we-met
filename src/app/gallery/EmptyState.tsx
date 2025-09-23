81e3d9aeb072b632da5ec9c1e2b86ee47130ef27
enrique <casillasenrique@live.com>
2025-09-23 00:57:54 -0400

    Fix more escaped characters

--- src/app/gallery/EmptyState.tsx
+++ src/app/gallery/EmptyState.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function EmptyState() {
   return (
     <div className="h-svh flex flex-col justify-center items-center gap-2 p-2 text-primary">
-      <p className="text-xl text-center">You haven't won any games yet! </p>
+      <p className="text-xl text-center">{"You haven't won any games yet!"} </p>
       {/* TODO: add link to today's game */}
       <Link className="text-lg underline underline-offset-4" href="/game">
         {"Try playing today's game."}
