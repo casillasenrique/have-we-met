a66ca1cc7cd28c2c12508a2cd7a6fb4e54f16028
enrique <casillasenrique@live.com>
2025-09-24 20:19:42 -0400

    Add SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import ClientLayout from "./components/ClientLayout";
 import { getTodaysGameId } from "@/api/objectData";
 import { Analytics } from "@vercel/analytics/next";
+import Head from "next/head";
+import { GAME_URL } from "@/utils/constants";
 
 /**
  * Ensure dynamic rendering for this and all routes.
@@ -11,6 +13,13 @@ import { Analytics } from "@vercel/analytics/next";
  * of build time, we set the layout to be dynamically rendered.
  */
 export const dynamic = "force-dynamic";
+
+const TITLE = "Have We Met?";
+const DESCRIPTION = "A game to guess artwork at the Met Museum in New York";
+const KEYWORDS =
+  "Met Museum, art, game, guessing game, artwork, bandle, wordle, puzzle, clues";
+const AUTHOR = "Enrique Casillas and Kelly Fang";
+const LARGE_IMAGE = "/main-image.jpg";
 
 export default function RootLayout({
   children,
@@ -22,6 +31,21 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content={KEYWORDS} />
+        <meta name="author" content={AUTHOR} />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:image" content={LARGE_IMAGE} />
+        <meta property="og:url" content={GAME_URL} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta name="twitter:image" content={LARGE_IMAGE} />
+      </Head>
       <body className="bg-white ">
         <div className="max-w-[640px] min-h-screen mx-auto bg-white outline outline-primary">
           <ClientLayout todaysGameId={todaysGameId}>{children}</ClientLayout>
